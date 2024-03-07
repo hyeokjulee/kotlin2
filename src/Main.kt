60fbfd2a2d58602b643694abf77e1fdeaf551071
HyeokJu Lee <dlhjwwe@naver.com>
2024-03-07 15:44:06 +0900

    상속 extends

--- src/Main.kt
+++ src/Main.kt
@@ -1,11 +1,24 @@
 fun main() {
-    var i = 10
-    var l = 20L
+}
 
-    var name = "10"
-    name = i.toString()
-    i = name.toInt()
+open class Person
 
-    l = i.toLong()
-    i = l.toInt()
+abstract class Animal {
+    open fun move() {
+        println("이동")
+    }
+}
+
+class SuperMan : Person()
+
+class Dog : Animal() {
+    override fun move() {
+        println("껑충")
+    }
+}
+
+class Cat : Animal() {
+    override fun move() {
+        println("살금")
+    }
 }
