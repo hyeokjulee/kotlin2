853e321ffb2a71cf9ba272e184865b8e18e052d5
HyeokJu Lee <dlhjwwe@naver.com>
2024-03-08 03:58:48 +0900

    강제 타입 변환 as

--- src/Main.kt
+++ src/Main.kt
@@ -1,36 +1,8 @@
 fun main() {
-    val dog: Animal = Dog()
-    val cat = Cat()
+    val box = Box(10)
+    val box2 = Box("dfdfd")
 
-    if (dog is Dog) {
-        dog.draw()
-        dog.move()
-        println("멍멍이")
-    }
+    println(box.value)
 }
 
-interface Drawable {
-    fun draw()
-}
-
-abstract class Animal {
-    open fun move() {
-        println("이동")
-    }
-}
-
-class Dog : Animal(), Drawable {
-    override fun move() {
-        println("껑충")
-    }
-
-    override fun draw() {
-        println("draw")
-    }
-}
-
-class Cat : Animal() {
-    override fun move() {
-        println("살금")
-    }
-}
+class Box<T>(val value: T)
